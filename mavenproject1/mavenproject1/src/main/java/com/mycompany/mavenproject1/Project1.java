4b92259216992abf406920df576170f2dd822c80
Chamodi Kaushalya <kaushalyachamo173@gmail.com>
2025-05-20 16:52:16 +0530

    Add registration form

---
+++ mavenproject1/mavenproject1/src/main/java/com/mycompany/mavenproject1/Project1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author u212005
+ */
+public class Project1{
+    
+    public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new RegistrationForm2().setVisible(true);
+        }
+    });
+    
+    
+}
+
+}
